0cd76b0e2315432d7f2e97faa8023bac92225016
rameezhm <rmughal246@yahoo.com>
2019-10-26 23:18:59 -0700

    11pm

--- main.js
+++ main.js
@@ -78,6 +78,8 @@ let presentData = function() {
     //populateProtein();
     populatevitaminD();
     populatefiber();
+
+    //fill in rest of populate functions
 }
 
 let populatePotassium = function() {
@@ -86,6 +88,24 @@ let populatePotassium = function() {
     document.getElementById('potassiumAmount').innerHTML = potassium[group-1] + " mg";
     for ( let i = 0; i < potassiumFoods.length; i++) {
         document.getElementById('potassiumFoodList').innerHTML += "<li>" + potassiumFoods[i] + "</li>";
+    }
+}
+
+let populatevitaminA = function() {
+    vitaminASection.style.visibility = "visible";
+    vitaminANav.style.visibility = "visible";
+    document.getElementById('vitaminAAmount').innerHTML = vitaminA[group-1] + " g";
+    for ( let i = 0; i < vitaminAFoods.length; i++) {
+        document.getElementById('vitaminAFoodList').innerHTML += "<li>" + vitaminAFoods[i] + "</li>";
+    }
+}
+
+let populatevitaminC = function() {
+    vitaminCSection.style.visibility = "visible";
+    vitaminCNav.style.visibility = "visible";
+    document.getElementById('vitaminCAmount').innerHTML = vitaminC[group-1] + " g";
+    for ( let i = 0; i < vitaminCFoods.length; i++) {
+        document.getElementById('vitaminCFoodList').innerHTML += "<li>" + vitaminCFoods[i] + "</li>";
     }
 }
 
@@ -107,6 +127,17 @@ let populatefiber = function() {
     }
 }
 
+let populateiron = function() {
+    ironNav.style.visibility = "visible";
+    ironSection.style.visibility = "visible";
+    document.getElementById('ironAmount').innerHTML = iron[group-1] + " g";
+    for ( let i = 0; i < ironFoods.length; i++) {
+        document.getElementById('ironFoodList').innerHTML += "<li>" + ironFoods[i] + "</li>";
+    }
+}
+
+//create the rest of the populate functions(just copy paste and replace vitamin name with new name)
+
 
 let bounded = function(x, y) {
     if (age >= x && age <= y) {
@@ -117,6 +148,8 @@ let bounded = function(x, y) {
 
 //bunch of nutritional data arrays
 //ordered in corresponding group numbers, starting with 1
+
+//just copy rows of vitamin from excel sheet
 let protein = [13,19,19,34,34,46,52,46,56,46,56,46,56]; //g of protien per day
 let potassium = ["3,000","3,800","3,800","4,500","4,500","4,700","4,700","4,700","4,700","4,700","4,700","4,700","4,700"]; //mg
 let vitaminD = [600,600,600,600,600,600,600,600,600,600,600,600,600];
@@ -124,6 +157,7 @@ let calcium = ["700","1,000","1,000","1,300","1,300","1,300","1,300","1,000","1,
 let iron = [7,10,10,8,8,15,11,18,8,18,8,8,8];
 let fiber = [];
 
+//put a few example foods
 let calciumFoods = ["Fortified ready-to-eat cereals (various)","Pasteurized processed American cheese","Parmesan cheese hard","Plain yogurt nonfat","Romano cheese","Almond milk (all flavors)","Pasteurized processed Swiss cheese","Tofu raw regular prepared with calcium sulfate","Gruyere cheese","Plain yogurt low-fat","Vanilla yogurt low-fat","Pasteurized processed American cheese food","Fruit yogurt low-fat","Orange juice calcium fortified","Soymilk (all flavors)","Ricotta cheese part skim","Swiss cheese","Evaporated milk","Sardines canned in oil drained","Provolone cheese","Monterey cheese","Mustard spinach (tendergreen) raw","Muenster cheese","Low-fat milk (1%)","Mozzarella cheese part-skim","Skim milk (nonfat)","Reduced fat milk (2%)","Colby cheese","Low-fat chocolate milk (1%)","Cheddar cheese","Rice drink","Whole buttermilk","Whole chocolate milk","Whole milk","Reduced fat chocolate milk (2%)","Ricotta cheese whole milk"];
 let potassiumFoods = ["Potato baked flesh and skin","Prune juice canned","Carrot juice canned","Passion-fruit juice yellow or purple","Tomato paste canned","Beet greens cooked from fresh","Adzuki beans cooked","White beans canned","Plain yogurt nonfat","Tomato puree","Sweet potato baked in skin","Salmon Atlantic wild cooked","Clams canned","Pomegranate juice","Plain yogurt low-fat","Tomato juice canned","Orange juice fresh","Soybeans green cooked","Chard swiss cooked","Lima beans cooked","Mackerel various types cooked","Vegetable juice canned","Chili with beans canned","Great northern beans canned","Yam cooked","Halibut cooked","Tuna yellowfin cooked","Acorn squash cooked","Snapper cooked","Soybeans mature cooked","Tangerine juice fresh","Pink beans cooked","Chocolate milk (1% 2% and whole)","Amaranth leaves cooked","Banana","Spinach cooked from fresh or canned","Black turtle beans cooked","Peaches dried uncooked","Prunes stewed","Rockfish Pacific cooked","Rainbow trout wild or farmed cooked","Skim milk (nonfat)","Refried beans canned traditional","Apricots dried uncooked","Pinto beans cooked","Lentils cooked","Avocado","Tomato sauce canned","Plantains slices cooked","Kidney beans cooked","Navy beans cooked"];
 let proteinFoods = [];
